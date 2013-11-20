11da763121a86bcb4bbbd9327d77bd7384aa505b
viniciusraupp <viniciusraupp@gmail.com>
2013-11-20 18:23:25 -0200

    Classe de teste de controle de Contrato criada.

---
+++ Trabalho/test/controle/ControleDoContratoTest.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package controle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Andre
+ * @author Vinícius
+ *
+ * 20/11/2013
+ *
+ */
+public class ControleDoContratoTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link controle.ControleDoContrato#getFranquiaPaginas()}.
+	 */
+	@Test
+	public void testGetFranquiaPaginas() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controle.ControleDoContrato#setFranquiaPaginas(int)}.
+	 */
+	@Test
+	public void testSetFranquiaPaginas() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controle.ControleDoContrato#imprimir(entidade.Impressora, int)}.
+	 */
+	@Test
+	public void testImprimir() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controle.ControleDoContrato#trocarToner(entidade.Impressora)}.
+	 */
+	@Test
+	public void testTrocarToner() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link controle.ControleDoContrato#trocarFusor(entidade.Impressora)}.
+	 */
+	@Test
+	public void testTrocarFusor() {
+		fail("Not yet implemented");
+	}
+
+}
