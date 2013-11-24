5edfc542f7730e4d214dd1c71fff06ae22f2bb99
André <Andr@192.168.1.101>
2013-11-24 21:43:25 -0200

    Passei PessoaFisica test para pacote "entidade" de test

---
+++ Trabalho/test/entidade/PessoaFisicaTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package entidade;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Andre e Vinícius
+ * 20/11/2013
+ */
+public class PessoaFisicaTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
