cb44dae7b7e03e3edf1fa763e92d60779d39d9fd
André <Andr@192.168.1.101>
2013-11-25 22:25:52 -0200

    Ajustei só texto com palavra errada. Nada de JAVA

--- Trabalho/test/entidade/ImpressoraTest.java
+++ Trabalho/test/entidade/ImpressoraTest.java
@@ -17,7 +17,7 @@ public class ImpressoraTest {
 	@Test
 	public void vidaUtilTest() {
 		impressora1.vidaUtil(100000);
-		assertEquals("Valor iniciua da vita util da impressora é 100000", 100000, impressora1.getVidaUtil());
+		assertEquals("Valor inicial da vida util da impressora é 100000", 100000, impressora1.getVidaUtil());
 
 	}
 
@@ -25,9 +25,9 @@ public class ImpressoraTest {
 	public void reduzVidaUtilTest() {
 		impressora1.vidaUtil(100000);
 		impressora1.reduzVidaUtil(25);
-		assertEquals("Valor atual da vita util da impressora é 100000-25", 99975, impressora1.getVidaUtil());
+		assertEquals("Valor atual da vida util da impressora é 100000-25", 99975, impressora1.getVidaUtil());
 		impressora1.reduzVidaUtil(75);
-		assertEquals("Valor atual da vita util da impressora é 99900", 99900, impressora1.getVidaUtil());
+		assertEquals("Valor atual da vida util da impressora é 99900", 99900, impressora1.getVidaUtil());
 
 	}
 
