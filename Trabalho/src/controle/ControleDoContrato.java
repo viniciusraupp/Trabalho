bbf65a0a06c5bfc4cb5352f197feab28e9250a84
André <Andr@192.168.1.101>
2013-11-26 23:27:16 -0200

    Ajustei só nomes, nada na estrutura.

--- Trabalho/src/controle/ControleDoContrato.java
+++ Trabalho/src/controle/ControleDoContrato.java
@@ -3,6 +3,7 @@ package controle;
 import entidade.Contrato;
 
 public class ControleDoContrato {
+	
 	private Contrato contrato;
 
 	public void adicionaContrato(String numContrato, String nome, String cnpj,
