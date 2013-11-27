4eb31542f036fab502cced9aa5a5f36e7fccad40
André <Andr@192.168.1.101>
2013-11-26 23:26:23 -0200

    Fiz varias tentativas, mas não deu...deixei algumas coisas comentadas, só por deixar.

--- Trabalho/test/controle/ControleDoContratoTest.java
+++ Trabalho/test/controle/ControleDoContratoTest.java
@@ -8,29 +8,48 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import entidade.Contrato;
+
 /**
+ * Classe de Teste do Controle do Contrato
+ * 
  * @author Andre
- * @author Vin√≠cius
+ * @author Vinicius
  *
- * 20/11/2013
+ * 26/11/2013
  *
  */
 public class ControleDoContratoTest {
 
-	/**
-	 * @throws java.lang.Exception
-	 */
+	
+	ControleDoContrato contrato;
+		
 	@Before
 	public void setUp() throws Exception {
+		
+		contrato = new ControleDoContrato();
+				
 	}
 
 	/**
-	 * Test method for {@link controle.ControleDoContrato#getFranquiaPaginas()}.
+	 * Teste Adiciona Contrato
 	 */
 	@Test
-	public void testGetFranquiaPaginas() {
-		fail("Not yet implemented");
+	public void testAdicionaContrato() {
+		contrato.adicionaContrato("0001", "ABC", "00023512000135", "0123ABC", "LIVRE", "LI02");
+		
+		//assertEquals("Numero do cOntrato deve ser 0001", "0001", contrato.adicionaContrato("0001", "ABC", "00023512000135", "0123ABC", "LIVRE", "LI02");
+		//assertEquals("Numero do cOntrato deve ser 0001", 0, contrato.equals(contrato));
 	}
+	
+	/**
+	 * Test franquia de p·ginas
+	 */
+	/*@Test
+	public void testGetFranquiaPaginas() {
+		contrato.setFranquiaPaginas(1000);
+		assertEquals("Franquia de p·ginas deve ser 1000", 1000, contrato.getFranquiaPaginas());
+	}*/
 
 	/**
 	 * Test method for {@link controle.ControleDoContrato#setFranquiaPaginas(int)}.
