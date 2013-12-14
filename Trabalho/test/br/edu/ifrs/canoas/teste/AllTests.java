cd0c0f48badedfd832bd3443204d92e98c8238cb
André <Andr@192.168.1.101>
2013-12-14 17:44:59 -0200

    Criado pacote para Suite Test, com AllTests

---
+++ Trabalho/test/br/edu/ifrs/canoas/teste/AllTests.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.edu.ifrs.canoas.teste;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * @author Andre e Vinicius
+ * 14/12/2013
+ */
+@RunWith(Suite.class)
+@SuiteClasses({br.edu.ifrs.canoas.teste.controle.AllTests.class,br.edu.ifrs.canoas.teste.entidade.AllTests.class})
+public class AllTests {
+
+}
