9a5a921f65c8d919fccf962c6141ae44b63d7fa9
André <Andr@192.168.1.101>
2013-12-14 17:39:58 -0200

    Incluida teste do contrato no alltests, estava faltando.

--- Trabalho/test/br/edu/ifrs/canoas/teste/entidade/AllTests.java
+++ Trabalho/test/br/edu/ifrs/canoas/teste/entidade/AllTests.java
@@ -10,7 +10,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(Suite.class)
 @SuiteClasses({ ImpressoraTest.class, PessoaFisicaTest.class,
-		PessoaJuridicaTest.class })
+		PessoaJuridicaTest.class, ContratoTest.class })
 public class AllTests {
 
 }
