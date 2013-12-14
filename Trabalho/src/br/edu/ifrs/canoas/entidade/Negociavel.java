5485d667669ea8c7f44f6cbd86ce7593bbe16fe8
André <Andr@192.168.1.101>
2013-12-14 17:53:03 -0200

    Ajuste da documentação, conforme relatorio do professor.

--- Trabalho/src/br/edu/ifrs/canoas/entidade/Negociavel.java
+++ Trabalho/src/br/edu/ifrs/canoas/entidade/Negociavel.java
@@ -3,12 +3,17 @@ package br.edu.ifrs.canoas.entidade;
 /**
  * Interface Negociavel
  * 
- * @author Andre e Vinícius 24/11/2013
+ * @author Andre e Vinícius
+ * 
+ * 24/11/2013
  */
+
 public interface Negociavel {
 
 	public void adicionaCliente(String nome, String cnpj);
+	
 	public void adicionaImpressoras(String numSerie, String marca,String modelo);
+	
 	public void setFranquiaPaginas(Integer franquiaPaginas);	
 
 }
