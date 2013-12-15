879f2c35822dfba7103e7a4f74d46c88861f86f3
André <Andr@192.168.1.101>
2013-12-15 20:24:58 -0200

    Ajustei construtor e coloquei setCpf não aceitando nulo e com tamanho 11

--- Trabalho/src/br/edu/ifrs/canoas/entidade/PessoaFisica.java
+++ Trabalho/src/br/edu/ifrs/canoas/entidade/PessoaFisica.java
@@ -14,17 +14,23 @@ public class PessoaFisica extends Cliente {
 	
 	//@Override
 	//public boolean adicionaCliente(String nome, String cpf) {
-	public PessoaFisica(String nome, String cpf){
-		if (nome!=null && nome!="" && cpf.length() == 11) {
+	//public PessoaFisica(String nome, String cpf){
+	public PessoaFisica(){
+		/*if (nome!=null && nome!="" && cpf.length() == 11) {
 			super.nome = nome;
-			this.cpf = cpf;
+			this.cpf = cpf;*/
 			//return true;
-		}
+		//}
 		//return false;		
 	}
 
 	public String getCPF() {
 		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		if (cpf!=null && cpf.length()==11)
+			this.cpf = cpf;
 	}
 
 	// @return the cpf no formato 333.333.333-22
