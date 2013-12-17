d413b98eef4250d8b1c92e551b47ad793c3ec084
André <Andr@192.168.1.101>
2013-12-16 22:01:02 -0200

    Testes concluidos

--- Trabalho/test/br/edu/ifrs/canoas/teste/entidade/PessoaJuridicaTest.java
+++ Trabalho/test/br/edu/ifrs/canoas/teste/entidade/PessoaJuridicaTest.java
@@ -17,45 +17,60 @@ import br.edu.ifrs.canoas.entidade.PessoaJuridica;
 public class PessoaJuridicaTest {
 
 	private PessoaJuridica cliente1;
-
+	
 	@Before
 	public void setUp() throws Exception {
 		cliente1 = new PessoaJuridica();
 	}
 
 	@Test
-	public void adicionaPessoaJuridicaTest() {
-
-		assertTrue("Nome de cliente valido e cnpj valido com 14 digitos",
-				cliente1.adicionaCliente("Fulano", "12345678912345"));
-		assertFalse(
-				"Nome de cliente valido e cnpj invalido, com menos de 14 digitos",
-				cliente1.adicionaCliente("Fulano", "123456789123"));
-		assertFalse(
-				"Nome de cliente valido e cnpj invalido, com mais de 14 digitos",
-				cliente1.adicionaCliente("Fulano", "1234567891234545"));
-		assertFalse("Nome de cliente vazio e cnpj valido",
-				cliente1.adicionaCliente("", "12345678912345"));
-		assertFalse("Nome de cliente valido e cnpj vazio",
-				cliente1.adicionaCliente("Fulano", ""));
+	public void setCpfTest(){
+		
+		cliente1.setCnpj(null);
+		assertNull("CPF deve ser nulo",cliente1.getCNPJ());
+		
+		cliente1.setCnpj("321");
+		assertNull("CPF deve ter 14 digitos",cliente1.getCNPJ());
+		
+		cliente1.setCnpj("12345678901234");
+		assertEquals("CPF é 12345678901234","12345678901234",cliente1.getCNPJ());
+		
+		cliente1.setCnpj("1234567890123456789");
+		//assertEquals("CPF é 12345678901","12345678901",cliente1.getCPF());
+		assertNull("CPF deve ter só 14 digitos",cliente1.getCNPJ());
 	}
 
 	@Test
-	public void getCNPJTest() {
-		cliente1.adicionaCliente("Fulano", "12345678912345");
-		assertEquals("CNPJ correto", "12345678912345", cliente1.getCNPJ());
-		assertNotEquals("CNPJ correto", "00045678912345", cliente1.getCNPJ());
-
+	public void getCPFFormatadoTest() {
+		cliente1.setCnpj("12345678901234");
+		assertEquals("cnpj correto", "12.345.678/9012-34",
+				cliente1.getCNPJFormatado());
+		assertNotEquals("cnpj correto", "01.023.456/6789-01",
+				cliente1.getCNPJFormatado());
 	}
-
+	
 	@Test
-	public void getCNPJFormatadoTest() {
-		cliente1.adicionaCliente("Fulano", "12345678912345");
-		assertEquals("CNPJ correto", "12.345.678.9123-45",
-				cliente1.getCNPJFormatado());
-		assertNotEquals("CNPJ correto", "123.456.789.123-45",
-				cliente1.getCNPJFormatado());
-
+	public void testGetCodigoFormatado() {
+		
+		cliente1.IncluiPessoaJuridica("LM Info", "12345678901234");
+		assertEquals("Código deve ser CLI0001","CLI0001",cliente1.getCodigoFormatado());
+		
+		cliente1.IncluiPessoaJuridica("LM Info", "12345678901234");
+		assertEquals("Código deve ser CLI0002","CLI0002",cliente1.getCodigoFormatado());
+		
+		for (int i=0;i<8;i++)
+			cliente1.IncluiPessoaJuridica("LM Info", "12345678901234");
+		assertEquals("Código deve ser CLI0010","CLI0010",cliente1.getCodigoFormatado());
+		
+		for (int i=0;i<90;i++)
+			cliente1.IncluiPessoaJuridica("LM Info", "12345678901234");
+		assertEquals("Código deve ser CLI0100","CLI0100",cliente1.getCodigoFormatado());
+		
+		for (int i=0;i<900;i++)
+			cliente1.IncluiPessoaJuridica("LM Info", "12345678901234");
+		assertEquals("Código deve ser CLI1000","CLI1000",cliente1.getCodigoFormatado());
+		
+		cliente1.IncluiPessoaJuridica("LM Info", "12345678901234");
+		assertEquals("Código deve ser CLI1001","CLI1001",cliente1.getCodigoFormatado());
 	}
-
 }
